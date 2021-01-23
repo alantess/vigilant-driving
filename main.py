f6139e86aa93ddb757ebf4fa217b18ff035ae95d
Alan <52587207+alantess@users.noreply.github.com>
2021-01-23 16:47:56 -0500

    ready to train

--- main.py
+++ main.py
@@ -1,169 +1,178 @@
-from gtrxl_torch.gtrxl_torch import GTrXL
-import math
-import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
-import torchvision
-import numpy as np
 import torch as T
-from torch import Tensor
+import cv2
+import numpy as np
+import pandas as pd
+import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.nn.modules import transformer
-import torchvision.transforms as transforms
+from gtrxl_torch.gtrxl_torch import GTrXL
+from sklearn.preprocessing import MinMaxScaler
+from torch import optim
+import torchvision
+import matplotlib.pyplot as plt
+from torchvision import transforms, models
 
 
-def make_patches(img: Tensor, patch_size: int) -> Tensor:
-    """
-    HW = Resolution of the image
-    P = Resolution of the patch
-    Calculate patch (S) = HW / P^2
-
-    :param img: Batch x Channels x Height x Width 
-    :return:Tensor: S x N x E
-    """
-    n, c = img.size(0), img.size(1)
-    height, width = img.size(2), img.size(3)
-    assert (height == width), "Height and width must be the same size"
-
-    n_patches = (height * width) // pow(patch_size, 2)
-    flatten_dim = c * pow(patch_size, 2)
-
-    flatten_img = T.zeros(size=(n_patches, n, flatten_dim), dtype=T.float)
-    n_row = math.sqrt(n_patches)
-
-    for i in range(n):
-        row, col = 0, 0
-        for j in range(n_patches):
-
-            if j % n_row == 0:
-                row += patch_size
-                col = patch_size
-            else:
-                col += patch_size
-
-            image_patch = img[i, :, row - patch_size:row, col - patch_size: col].reshape(-1)
-            flatten_img[j][i] = image_patch
-
-    return flatten_img
-
-
-def load_dataset(train_path, test_path, batch_size=32):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize(64),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-    trainset = torchvision.datasets.ImageFolder(root=train_path, transform=transform)
-    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
-
-    testset = torchvision.datasets.ImageFolder(root=test_path, transform=transform)
-    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
-
-    return trainloader, testloader
-
-
-class MarkovNet(nn.Module):
-    def __init__(self, d_model, n_heads, n_layers, n_classes, h_dims, dropout=0.5, lr=0.00025):
-        super(MarkovNet, self).__init__()
-        self.transformer = GTrXL(d_model, n_heads, n_layers, activation="gelu")
-        self.dropout = nn.Dropout(dropout)
-        self.fc1 = nn.Linear(d_model, h_dims)
-        self.out = nn.Linear(h_dims, n_classes)
+class CNNGRU(nn.Module):
+    def __init__(self, lr=0.00065):
+        super(CNNGRU, self).__init__()
+        self.base_model = models.resnet50(pretrained=True)
+        self.fc1 = nn.Linear(1000, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.gru = nn.GRU(128, 256, 4)
+        self.out = nn.Linear(256, 1)
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
 
-    def save(self, path):
-        T.save(self.state_dict(), path)
+    def forward(self, x):
+        x = self.base_model(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = x.unsqueeze(1)
+        out, h0 = self.gru(x)
+        out = self.out(out).mean(1)
+        return out
+
+
+class CNNTransformer(nn.Module):
+    def __init__(self, lr=0.00045):
+        super(CNNTransformer, self).__init__()
+        self.base_model = models.resnet50(pretrained=True)
+        self.fc1 = nn.Linear(1000, 256)
+        self.tranformer = GTrXL(256, 4, 3)
+        self.fc2 = nn.Linear(256, 512)
+        self.dropout = nn.Dropout(0.2)
+        self.out = nn.Linear(512, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
 
     def forward(self, x):
-        x = self.transformer(x)
-        x = F.relu(self.dropout(self.fc1(x)))
-        x = self.out(x)
+        x = self.base_model(x)
+        x = F.relu(self.fc1(x))
+        x = self.tranformer(x)
+        x = F.relu(self.dropout(self.fc2(x)))
+        x = self.out(x).mean(0)
         return x
 
 
-def calculate_d_model(trainloader, patches):
-    img, _ = next(iter(trainloader))
-    img = make_patches(img, patches)
-    return img.size(2)
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def get_labels(dir):
+    df = pd.read_csv(dir)
+    scaler = MinMaxScaler()
+    scaler.fit(df.values)
+    data = scaler.transform(df.values)
+    return scaler , data
 
 
-def train(train_dir, test_dir, epochs, device, nheads=4, t_layers=3, h_dims=256, patch_dims=16, train=True):
-    trainloader, testloader = load_dataset(train_dir, test_dir, BATCH_SIZE)
-    d_model = calculate_d_model(trainloader, 16)
-    n_classes = 2
-    best_score = np.inf
+def vid_to_tensor(dir, transform):
+    image_tensor = T.zeros((20400, 3, 256, 256))
+    video = cv2.VideoCapture(dir)
+    success, image = video.read()
+    count = 0
+    while success:
+        image_tensor[count] = transform(image)
+        success, image = video.read()
+        count += 1
 
-    criterion = nn.CrossEntropyLoss()
-    model = MarkovNet(d_model, nheads, t_layers, n_classes, h_dims).to(device)
-    print("Train loader size: ", len(trainloader))
-    print("Model Params: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+    return image_tensor
 
-    if train:
-        print("Train mode initiated.")
-        for epoch in range(epochs):
-            running_loss = 0.0
-            total_loss = 0.0
-            correct = 0
-            total = 0
-            for i, data in enumerate(trainloader, 0):
-                inputs, labels = data
-                img = make_patches(inputs, patch_dims)
-                img, labels = img.to(device), labels.to(device)
+def train(train_dir,labels_dir,  transform,criterion, batch, EPOCHS =1,gru = True):
+    device = T.device("cuda") if T.cuda.is_available() else T.device("cpu")
+    best_loss = np.inf
+    if gru:
+        model = CNNGRU().to(device)
+        save_path = "models/gru.pt"
+    else:
+        model = CNNTransformer().to(device)
+        save_path = "models/transformer.pt"
 
+    print("Model Parameters", sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    scaler, labels = get_labels(labels_dir)
+    labels = T.tensor(labels, dtype=T.float, device=device)
+    
+    print("starting...")
+    for epoch in range(EPOCHS):
+        total_loss , running_loss = 0.0, 0.0
+        image_tensor = T.zeros((batch, 3,256,256), device=device)
+        video = cv2.VideoCapture(train_dir)
+        success, image = video.read()
+        index,i,count  = 0,0,0
+        counter = 0
+        while success: 
+            image_tensor[count] = transform(image) 
+            count += 1
+
+            if count % batch == 0: 
+                i += 1
+                index = i * batch
+                # Set labels
+                y = labels[index-batch:index] 
+                
+                # # zeros out gradients
                 for p in model.parameters():
                     p.grad = None
-
-                output = model(img).mean(0)
-
-                loss = criterion(output, labels)
+                #
+                # Make a predictions and get the loss
+                pred = model(image_tensor)
+                loss = criterion(pred,y)
                 loss.backward()
+                # Optimizer step
                 model.optimizer.step()
 
                 running_loss += loss.item()
+                total_loss += running_loss
+                if i % 255 == 254: 
+                    running_loss /= 255
+                    print(f"[{epoch}/{i+1}] \tLoss {running_loss:.3f} ")
 
-                if i % 69 == 68:
-                    print(f"|{epoch}|{i + 1}|\tloss: {running_loss / 69:.3f}")
-                    total_loss += running_loss
-                    running_loss = 0.0
+                image_tensor = T.zeros((batch, 3,256,256), device=device)
+                count = 0
 
-            if total_loss < best_score:
-                print("Saving...")
-                best_score = total_loss
-                model.save("markov_net.pt")
-
-            if epoch % 5 == 0:
-                with T.no_grad():
-                    for j , info in enumerate(testloader, 0):
-                        input, label = info
-                        image = make_patches(input,patch_dims)
-                        image, label = image.to(device), label.to(device)
-                        out = model(image).mean(0)
-                        _, predicted = T.max(out.data, 1)
-                        correct += T.sum(predicted == label)
-                        total += label.size(0)
-
-                        acc = (correct / total) * 100
-                    print(f"Accuracy on val set: {acc:.2f}% ")
+            success, image = video.read()
 
 
+        print("Total loss: ", total_loss )
 
-        print("FINISHED.")
-    else:
-        print("Test mode initiated.")
+        if total_loss < best_loss:
+            print("Saving...")
+            best_loss = total_loss
+
+    # imshow(torchvision.utils.make_grid(image_tensor.cpu()))
+
 
 
 if __name__ == '__main__':
-    EPOCHS = 100
-    BATCH_SIZE = 32
-    SEED = 75
-    T.backends.cudnn.benchmark = True
+    train_video = "/mnt/d/pytorch/speedchallenge/data/train.mp4"
+    train_labels = "/mnt/d/pytorch/speedchallenge/data/train.txt"
+
+    SEED = 98
     T.manual_seed(SEED)
     np.random.seed(SEED)
-
+    T.backends.cudnn.benchmark = True
+    best_loss = np.inf
+    BATCH_SIZE = 16
+    EPOCHS = 200
     device = T.device("cuda") if T.cuda.is_available() else T.device("cpu")
-    train_dir = '../../dataset/ltc_classifer'
-    test_dir = '../../dataset/ltc_test_classifier'
 
-    train(train_dir, test_dir, EPOCHS, device, t_layers=4)
+    criterion = nn.MSELoss()
+
+    transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((256,256)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+        ])
+
+    
+
+    train(train_video,train_labels,transform, criterion, 20)
+
+
+
+
+
