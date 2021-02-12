697ea67cb1e6564ac12eae737a613ac6601c22b9
alant <alantessier97@gmail.com>
2021-02-12 16:38:06 -0500

    Camera can now be opened on main page

--- mobile/app/src/main/java/com/example/vigilant/MainActivity.kt
+++ mobile/app/src/main/java/com/example/vigilant/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.example.vigilant
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.renderscript.ScriptGroup
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import kotlinx.android.synthetic.main.activity_main.*
+typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+        //  Switches the Content View
+
+
     }
+
+    fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        var preview : Preview = Preview.Builder()
+                .build()
+
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+        preview.run {
+
+            setSurfaceProvider(previewView?.surfaceProvider)
+        }
+
+       cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
 }
---
+++ mobile/app/src/main/java/com/example/vigilant/Visuals.kt
@@ -0,0 +1,4 @@
+package com.example.vigilant
+
+class Visuals {
+}
