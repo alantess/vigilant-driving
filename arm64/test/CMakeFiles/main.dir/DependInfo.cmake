59feecf125977b7cb5e0a1ed64924721bb4b23e5
Alan <alantessier97@gmail.com>
2021-03-10 13:06:11 -0500

    cleaned up code

---
+++ arm64/test/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alan/Code/vigilant-driving/arm64/src/camera.cpp" "/home/alan/Code/vigilant-driving/arm64/test/CMakeFiles/main.dir/src/camera.cpp.o"
+  "/home/alan/Code/vigilant-driving/arm64/src/main.cpp" "/home/alan/Code/vigilant-driving/arm64/test/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/opencv4"
+  "/home/alan/Downloads/libtorch/include"
+  "/home/alan/Downloads/libtorch/include/torch/csrc/api/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
